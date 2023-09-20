8ca2cd338e244a7fe5603c6d613d233208ec7cc6
CuriousAdventurer <138018696+CuriousAdventurer@users.noreply.github.com>
2023-09-20 15:33:54 +0300

    Add files via upload

---
+++ dz5/zd 36/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+// МАССИВ НАЧИНАЕТСЯ С 0!!!!!!!!!!!!!!
+Console.WriteLine("Сколько чисел в массиве нужно?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Создал для Вас массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int num = 1; num < numbers.Length; num+=2)
+    sum = sum + numbers[num];
+
+    Console.WriteLine($"В массиве {numbers.Length} числа, сумма элементов, стоящих на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ dz5/zd34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Сколько чисел в массиве нужно?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Создал для Вас массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int num = 0; num < numbers.Length; num++)
+if (numbers[num] % 2 == 0)
+count++;
+
+Console.WriteLine($"В массиве {numbers.Length} чисел, {count} из них являются чётными");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}  
